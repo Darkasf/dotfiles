a9fb5fa7008e31a3ad82582c0c900aaa13ee774b
Emilian Frauhammer <emilian.frauhammer@protonmail.com>
2019-06-19 13:25:54 +0200

    zshrc

---
+++ .alias.sh
@@ -0,0 +1,23 @@
+# aliases
+alias cl="clear"
+cda() {
+	cd $1;
+	ls --color=auto;
+}
+alias cd='cda'
+cls() {
+	clear;
+	ls --color=auto;
+}
+alias v="vim"
+alias rm="rm -I"
+alias bc="bc -ql"
+alias mutt="neomutt"
+alias cfi="vim ~/.config/i3/config"
+cdmkdir() {
+	mkdir -p "$1";
+	cd "$1";
+}
+alias cmkdir="cdmkdir"
+alias py="python"
+alias nuke="killall -9"
